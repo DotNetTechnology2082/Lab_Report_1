8a9ef68a7b544d360dc06bad968124ed8334d6ff
Neha Mahto <mahtoneha2555@gmail.com>
2026-03-24 20:45:26 +0545

    Lab 1 Code

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Employee
+{
+    public int ID;
+    public string Name;
+    public double Salary;
+
+    public void Display()
+    {
+        Console.WriteLine("Employee ID: " + ID);
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Salary: " + Salary);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee emp = new Employee();
+        
+        emp.ID = 101;
+        emp.Name = "Neha";
+        emp.Salary = 50000;
+
+     
+        emp.Display();
+
+        Console.ReadLine();
+    }
+}
