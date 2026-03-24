37197eff429f2e9deb90ac24fb47b439ccb59fc6
shudhansubca23-ship-it <shudhansubca23@oic.edu.np>
2026-03-24 22:00:23 +0545

    hello1

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Employee
+{
+    // Data members
+    public int ID;
+    public string Name;
+    public double Salary;
+
+    // Method to display employee details
+    public void Display()
+    {
+        Console.WriteLine("Employee ID: " + ID);
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Salary: " + Salary);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating object of Employee class
+        Employee emp = new Employee();
+
+        // Assigning values
+        emp.ID = 101;
+        emp.Name = "Shudhansu Shah";
+        emp.Salary = 100000;
+
+        // Displaying employee details
+        emp.Display();
+    }
+}
