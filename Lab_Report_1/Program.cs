569839274ced401f5b91fd7ff441d3c0ddebedde
Tashi Sherpa <sherpajack3@gmail.com>
2026-03-24 21:00:28 +0545

    Pushed code of Lab1

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Employee
+{
+    public int ID;
+    public string Name;
+    public double Salary;
+
+    public void Display()
+    {
+        Console.WriteLine("Employee ID: " + ID);
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Salary: " + Salary);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee emp = new Employee();
+
+        emp.ID = 201;
+        emp.Name = "Tasi Sherpa";
+        emp.Salary = 10000000;
+
+
+        emp.Display();
+    }
+}
