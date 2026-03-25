ef5cdbea33a129893a450e5cfe839d28fe0588d0
Anmol99914 <giria4621@gmail.com>
2026-03-25 22:35:10 +0545

    Employee class created with object and method:)

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Write a C# program to define a class Employee with data members (ID, Name, Salary).
+// Create an object of the class and display the employee details.
+using System; 
+// is used to include the System namespace in a C# program, which provides basic classes like Console, String, Math, etc.
+class Employee { 
+    public int id;
+    public string name;
+    public double salary;
+
+    public Employee(int id, string name, double salary) { 
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void EmployeeDetails() { 
+        Console.WriteLine($"Id: {id}, Name: {name}, Salary: Rs {salary}");  
+    }
+}
+class Program { 
+    public static void Main(string[] args) { 
+        Employee emp = new Employee(1, "Anmol", 50000);
+        emp.EmployeeDetails();
+    }
+}
