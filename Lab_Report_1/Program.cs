75a3104385f1875c8b292fd202c66093b0b0e180
prasansha.bhusal <pbhusal457@gmail.com>
2026-03-25 09:08:00 +0545

    Lab 1

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class employee
+{
+    public int id;
+    public string name;
+    public double salary;
+
+    public void dislayinfo()
+    {
+        Console.WriteLine("id is" + id);
+        Console.WriteLine("name " + name);
+        Console.WriteLine("salary is" + salary);
+    }
+}
+class lab1
+{
+    static void Main(string[] args)
+    {
+        employee e = new employee();
+        e.id = 22;
+        e.name = "Prasansha Bhusal";
+        e.salary = 22000;
+
+        e.dislayinfo();
+    }
+}
