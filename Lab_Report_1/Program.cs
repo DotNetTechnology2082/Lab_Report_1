929f1e53efa3f6d47ac6346fb5b314a5d028285e
krishal_11 <krishalbca23@oic.edu.np>
2026-03-25 09:23:03 +0545

    Updated

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Employee
+{
+    public int ID;
+    public string Name;
+    public double Salary;
+
+    public void Display()
+    {
+        Console.WriteLine("Employee Details:");
+        Console.WriteLine("ID: " + ID);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Salary: " + Salary);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee emp = new Employee();
+
+        emp.ID = 11;
+        emp.Name = "Krishal";
+        emp.Salary = 30000;
+
+        emp.Display();
+
+        Console.ReadLine();
+    }
+}
