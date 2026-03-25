83d87395da9dba5524bbba1741fe0587fd5263bb
Anujbh281 <anujbh281@gmail.com>
2026-03-25 13:39:55 +0545

    AnujBh

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Employee
+{
+    // Data members (fields / properties)
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; set; }
+
+    // Method to display employee details
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Employee Details:");
+        Console.WriteLine("ID: " + ID);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Salary: " + Salary);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiate object of Employee class
+        Employee emp1 = new Employee();
+
+        // Assign values
+        emp1.ID = 101;
+        emp1.Name = "Anuj Bhattarai";
+        emp1.Salary = 50000;
+
+        // Display details
+        emp1.DisplayDetails();
+    }
+}
