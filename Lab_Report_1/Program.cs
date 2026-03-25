dc35e3e4925af5c964fd88182d16f90c5f750611
rahulbca23-tech <rahulbca23@oic.edu.np>
2026-03-25 08:49:24 +0545

    Tested if and else if

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter student marks: ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+
+        if (marks >= 40)
+        {
+            Console.WriteLine("Pass!!");
+        }
+        else
+        {
+            Console.WriteLine("Fail!!");
+        }
+    }
+}
