41d03ea22aaee05acdd3ff3cfbd3377a10dac810
swastika.bist <swastikabca23@oic.edu.np>
2026-03-26 07:47:17 +0545

    lab1

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class employee
+{
+    public int id;
+    public string name;
+    public double salary;
+
+    public void dislayinfo()
+    {
+        Console.WriteLine("id is" + id);
+        Console.WriteLine("name " + name);
+        Console.WriteLine("salary is" + salary);
+    }
+}
+class lab1
+{
+    static void Main(string[] args)
+    {
+        employee e = new employee();
+        e.id = 33;
+        e.name = "swastika bist";
+        e.salary = 33000;
+
+        e.dislayinfo();
+    }
+}
