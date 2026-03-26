de48c0418521847d2d15403891e2c1d06305bcef
dibyansbca23-byte <dibyansbca23@oic.edu.np>
2026-03-26 08:05:12 +0545

    hello

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -2,12 +2,10 @@
 
 class Employee
 {
-    // Data members
     public int ID;
     public string Name;
     public double Salary;
-
-    // Method to display employee details
+    
     public void Display()
     {
         Console.WriteLine("Employee ID: " + ID);
@@ -20,15 +18,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creating object of Employee class
         Employee emp = new Employee();
-
-        // Assigning values
+        
         emp.ID = 101;
         emp.Name = "John Doe";
         emp.Salary = 50000;
-
-        // Displaying details
+        
         emp.Display();
 
         Console.ReadLine();
