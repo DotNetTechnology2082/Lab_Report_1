b913e567362f36186fcd5ce6e1604d417f34088c
aayushadev <aayushabca23@oic.edu.np>
2026-03-26 08:12:17 +0545

    lab1

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class employee
+{
+    public int id;
+    public string name;
+    public double salary;
+
+    public void dislayinfo()
+    {
+        Console.WriteLine("id is" + id);
+        Console.WriteLine("name " + name);
+        Console.WriteLine("salary is" + salary);
+    }
+}
+class lab1
+{
+    static void Main(string[] args)
+    {
+        employee e = new employee();
+        e.id = 01;
+        e.name = "Aayusha Dahal";
+        e.salary = 100000;
+
+        e.dislayinfo();
+    }
+}
