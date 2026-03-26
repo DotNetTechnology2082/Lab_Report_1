2656a8ab04a4bc52668a79aadbbeca5a9ad272ac
Pradip Prajapati <pradipbca23@oic.edu.np>
2026-03-26 08:21:12 +0545

    lab1

--- Lab_Report_1/Program.cs
+++ Lab_Report_1/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter student marks:");
+//            int marks = Convert.ToInt32(Console.ReadLine());
+
+//        if (marks >= 50)
+//        {
+//            Console.WriteLine("Pass");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Fail");
+//        }
+//    }
+//}
+using System;
+
+class Employee
+{
+    public int ID;
+    public string Name;
+    public double Salary;
+
+    public void Display()
+    {
+        Console.WriteLine("Employee Details:");
+        Console.WriteLine("ID: " + ID);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Salary: " + Salary);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee emp = new Employee();
+
+        emp.ID = 21;
+        emp.Name = "Pradip Prajapati";
+        emp.Salary = 30000;
+
+        emp.Display();
+
+        Console.ReadLine();
+    }
+}
